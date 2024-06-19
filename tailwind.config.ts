4f04fbe31516a649dec2c896f610daded30cac62
Shameem A R <mobihack.me@gmail.com>
2024-06-19 21:56:42 +0530

    fix: improve tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./src/components/**/*.{ts,tsx}",
+    "./src/containers/**/*.{ts,tsx}",
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/views/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
