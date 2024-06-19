210af113170c20767173286f4f0ff7739ee0ce66
Shameem A R <mobihack.me@gmail.com>
2024-06-19 21:51:46 +0530

    init: initial design for login page

--- src/views/auth/LoginPage/LoginPage.tsx
+++ src/views/auth/LoginPage/LoginPage.tsx
@@ -1,3 +1,60 @@
+import { Button } from "@/components";
+import { CONFIG } from "@/config";
+import { FormField } from "@/containers";
+import clsx from "clsx";
+import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+
 export const LoginPage = (): JSX.Element => {
-  return <div className="bg-red-500">Hello World</div>;
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<any> = (data) => console.log(data);
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center h-96">
+      <div
+        className={clsx(
+          "max-w-sm w-full",
+          "border border-gray",
+          "rounded-lg p-4"
+        )}
+      >
+        <h1 className="text-center text-xl text-gray-800 font-medium mb-2">
+          Login to {"TaskBase™"}
+        </h1>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mt-4">
+            <FormField
+              {...register("email")}
+              label="Email Address"
+              placeholder="john@example.com"
+              className="mb-2"
+            />
+            <FormField
+              {...register("password")}
+              type="password"
+              label="Password"
+              placeholder="•••••••••••••••••"
+            />
+
+            <Button width="full" className="mt-6">
+              Login
+            </Button>
+          </div>
+        </form>
+      </div>
+      <p className="text-sm text-gray-600 mt-4">
+        New to {"TaskBase™"}?{" "}
+        <Link className="text-brand-main underline" href="/auth/signup">
+          Create an Account
+        </Link>
+      </p>
+    </div>
+  );
 };
