fafce75c4b39d4444e029d27abf4163c87d788f3
Shameem A R <mobihack.me@gmail.com>
2024-06-20 16:04:50 +0530

    feat: add new component method and error, helperText props to FormField

--- src/containers/FormField/FormField.tsx
+++ src/containers/FormField/FormField.tsx
@@ -1,26 +1,60 @@
-import { Input, InputLabel, InputProps } from "@/components";
+import {
+  Input,
+  InputLabel,
+  InputProps,
+  Textarea,
+  TextareaProps,
+} from "@/components";
+import clsx from "clsx";
 
-interface Props extends InputProps {
+type ComponentProps = TextareaProps & InputProps;
+
+interface Props extends ComponentProps {
   label: string;
   name: string;
   inputClassName?: string;
   labelClassName?: string;
+  component?: "input" | "textarea";
+
+  helperText?: string;
+  error?: boolean;
 }
 
 export const FormField = ({
   label,
   name,
   className,
+  component = "input",
   inputClassName,
   labelClassName,
+  helperText,
+  error = false,
   ...props
 }: Props): JSX.Element => {
+  const Component = component === "input" ? Input : Textarea;
+
   return (
     <div className={className}>
       <InputLabel htmlFor={name} className={labelClassName}>
         {label}
       </InputLabel>
-      <Input id={name} name={name} className={inputClassName} {...props} />
+      <Component
+        error={error}
+        id={name}
+        name={name}
+        className={inputClassName}
+        {...props}
+      />
+      {helperText && (
+        <p
+          className={clsx(
+            "px-3 text-gray-500 text-xs mt-1",
+            error && "text-red-500"
+          )}
+        >
+          {helperText}
+        </p>
+      )}
     </div>
   );
 };
